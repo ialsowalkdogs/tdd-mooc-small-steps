8c53521e1fd94b2c11cdc4fa811bb85ae7a1763b
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-02-16 09:50:56 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -1,5 +1,6 @@
 import "./polyfills.mjs";
 import express from "express";
+import { Temporal } from "@js-temporal/polyfill";
 
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
@@ -29,7 +30,7 @@ function createApp(database) {
     }
   }
 
-  function parsePlainDate(dateString) { return null;}
+  function parsePlainDate(dateString) { return Temporal.PlainDate.from(dateString); }
 
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
