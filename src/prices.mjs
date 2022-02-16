848c272ea883d9ae0016c19b606860ce03410a48
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-02-16 09:58:54 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -71,7 +71,7 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date) {
+  function calculateReduction(date, newDate) {
     let reduction = 0;
     if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
