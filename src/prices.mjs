7ecee9bff402c1e549aeeb93b194d5f1ed4dbd7b
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-02-16 10:07:02 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -86,7 +86,6 @@ function createApp(database) {
   function isHoliday(date, newDate) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
       let newHoliday = Temporal.PlainDate.from(row.holiday);
       if (
         newDate &&
