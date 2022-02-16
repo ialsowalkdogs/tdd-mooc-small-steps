6c510082833b634a089a756df2fbbfbdf720d277
Olga Vorozheykina <olga.vorozheykina@relexsolutions.com>
2022-02-16 09:57:34 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -21,7 +21,7 @@ function createApp(database) {
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
     const newDate = parsePlainDate(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, date, baseCost, newDate);
     res.json({ cost });
   });
 
